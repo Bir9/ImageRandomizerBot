e1b09c71b443513f4409657537cb10111c3e26d8
Adib Shargh <dopeman7123@gmail.com>
2022-01-13 11:22:42 -0500

    first working versiom

--- imagemanipulator.py
+++ imagemanipulator.py
@@ -1,31 +1,33 @@
 from PIL import Image, ImageEnhance, ImageFile
-import random, shutil, os
+import random, shutil, os, os.path
 ImageFile.LOAD_TRUNCATED_IMAGES = True
+
 def change_contrast(img, level):
     factor = (259 * (level + 255)) / (255 * (259 - level))
     def contrast(c):
         return 128 + factor * (c - 128)
     return img.point(contrast)
 
-
-randint = random.randint(1, 6) #TODO: Make max limit the amount of files inside the "images" folder
+randint = random.randint(1, 7) #TODO: Make max limit the amount of files inside the "images" folder
 rand = str(randint)
-randSaturation = random.randint(0, 55)
-randBrightness = random.uniform(0, 2)
-randContrast = random.randint(0, 255)
-file = './images/' + rand + '.png'
-im = Image.open(file)
-os.remove('processedImages/done.png')
-os.remove('processedImages/done2.png')
-os.remove('processedImages/done3.png')
+def manipultor():
+    randSaturation = random.randint(0, 55)
+    randBrightness = random.uniform(0, 2)
+    randContrast = random.randint(0, 255)
+    file = './images/' + rand + '.png'
+    im = Image.open(file)
+    os.remove('processedImages/done.png')
+    os.remove('processedImages/done2.png')
+    os.remove('processedImages/done3.png')
+    converterSaturation = ImageEnhance.Color(im)
+    new_image = converterSaturation.enhance(randSaturation)
+    new_image.save("processedImages/done.png")
+    converterBrightness = ImageEnhance.Brightness(Image.open('processedImages/done.png'))
+    new_image = converterBrightness.enhance(randBrightness)
+    new_image.save("processedImages/done2.png")
+    converterContrast = change_contrast(Image.open('processedImages/done2.png'), randContrast)
+    new_image = converterContrast
+    new_image.save("processedImages/done3.png")
+    #please don't ask what this mess of code is
 
-converterSaturation = ImageEnhance.Color(im)
-new_image = converterSaturation.enhance(randSaturation)
-new_image.save("processedImages/done.png")
-converterBrightness = ImageEnhance.Brightness(Image.open('processedImages/done.png'))
-new_image = converterBrightness.enhance(randBrightness)
-new_image.save("processedImages/done2.png")
-converterContrast = change_contrast(Image.open('processedImages/done2.png'), randContrast)
-new_image = converterContrast
-new_image.save("processedImages/done3.png")
-#please don't ask what this mess of code is
+manipultor()
--- twitterApi.py
+++ twitterApi.py
@@ -1,4 +1,4 @@
-import tweepy
+import tweepy, imagemanipulator
 
 consumer_key = 'xxxx'
 consumer_secret = 'xxxx'
@@ -8,6 +8,15 @@ access_token_secret = 'xxxx'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth, wait_on_rate_limit=True)
+api = tweepy.API(auth)
+
+image = open('processedImages/done3.png', 'rb')
+file = 'processedImages/done3.png'
+imagemanipulator.manipultor()
+
+
+media = api.media_upload(filename = file)
+tweet = imagemanipulator.rand
+post_result = api.update_status(status = tweet, media_ids =[media.media_id])
 
 
